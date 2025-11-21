c67edefea99f54fd6e995cec77752c60c107e7d9
kenneth-kipkosgei <kennethkosgei38@gmail.com>
2025-11-21 20:58:55 +0300

    Implement EmailAddressParser with regex pattern matching

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,9 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_string):
+        self.email_string = email_string
+    
+    def parse(self):
+        emails = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', self.email_string)
+        return sorted(list(set(emails)))
